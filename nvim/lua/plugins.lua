276130a4913de11d029dbbeb288039e78a2a03a3
Jacob Matthews <jacob@jacobian.com.au>
2022-11-10 14:08:01 +1100

    Add PHP Language Server

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -128,6 +128,7 @@ return require("packer").startup(function(use)
 					client.server_capabilities.documentFormattingProvider = false -- Use null-ls prettierd
 				end,
 			})
+			lspconfig.phpactor.setup({})
 			lspconfig.cssls.setup({
 				capabilities = capabilities,
 				on_attach = function(client)
