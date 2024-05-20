88e930a7823fc72a687fd06f090a00e63ac309e8
Jacob Ian Matthews <jacob@jacobian.com.au>
2024-05-20 10:57:30 +1000

    Add keymaps for snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -9,5 +9,23 @@ return {
     }
     local loader = require "luasnip.loaders.from_lua"
     loader.lazy_load { paths = { "~/.config/nvim/snippets", "~/snippets" } }
+
+    vim.keymap.set({ "i", "s" }, "<c-l>", function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      end
+    end, { silent = true })
+
+    vim.keymap.set({ "i", "s" }, "<c-h>", function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      end
+    end, { silent = true })
+
+    vim.keymap.set({ "i", "s" }, "<c-e>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end)
   end,
 }
