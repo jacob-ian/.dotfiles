3c379486fef3f58dab5df2fa806072bacc4e1a55
Jacob Matthews <jacob@jacobmatthe.ws>
2024-05-26 21:20:35 +1000

    Update to gruvbox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
   {
-    "AlexvZyl/nordic.nvim",
+    "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("nordic").load {}
+      vim.o.background = "dark"
+      vim.cmd "colorscheme gruvbox"
     end,
   },
+  "navarasu/onedark.nvim",
+  "AlexvZyl/nordic.nvim",
   "neanias/everforest-nvim",
   "projekt0n/github-nvim-theme",
   "rebelot/kanagawa.nvim",
