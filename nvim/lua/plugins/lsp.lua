7889b8a90f6b203b6155cbce4161e8769608ad66
Jacob Matthews <jacob@jacobmatthe.ws>
2025-11-02 11:35:07 +1100

    update formatting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -117,19 +117,34 @@ return {
 
       -- FORMATTING
       local conform = require "conform"
+
+      local formatWithBiomeOrPrettier = function(bufnr)
+        if conform.get_formatter_info("prettier", bufnr).available then
+          return { "prettier" }
+        else
+          return { "biome", "biome-organize-imports" }
+        end
+      end
+
       conform.setup {
+        formatters = {
+          prettier = {
+            require_cwd = true,
+          },
+        },
         formatters_by_ft = {
           lua = { "stylua" },
-          html = { "prettierd", "prettier", stop_after_first = true },
-          yaml = { "prettierd", "prettier", stop_after_first = true },
           markdown = { "prettierd", "prettier", stop_after_first = true },
-          templ = { "templ" },
-          sql = { "sleek" },
+          yaml = { "prettierd", "prettier", stop_after_first = true },
+          typescript = formatWithBiomeOrPrettier,
+          typescriptreact = formatWithBiomeOrPrettier,
+          json = formatWithBiomeOrPrettier,
+          graphql = formatWithBiomeOrPrettier,
+          html = formatWithBiomeOrPrettier,
         },
         format_on_save = {
           timeout_ms = 500,
           lsp_format = "fallback",
-          stop_after_first = true,
         },
       }
 
