dec6158353da0a67a259f83d23b6d767ef296158
Jacob Matthews <jacob@jacobmatthe.ws>
2026-06-10 16:45:08 +1000

    fix terraform ls

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -25,8 +25,16 @@ vim.lsp.enable {
 }
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function()
+  callback = function(args)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+
+    -- terraform-ls overflows deltaStartChar in semantic tokens for files
+    -- with heredocs (terraform-ls#2122), breaking the highlighter. Drop the
+    -- semantic-token layer; completion/diagnostics/hover are unaffected.
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.name:match("terraform") then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
   end,
 })
