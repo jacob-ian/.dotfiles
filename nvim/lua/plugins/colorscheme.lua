92dfd35720e4e8d147d118fa179269ab45220b42
Jacob Matthews <jacob@jacobmatthe.ws>
2026-07-01 14:22:02 +1000

    kanso theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,31 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("catppuccin").setup {
-        auto_integrations = true,
-        color_overrides = {
-          mocha = {
-            base = "#000000",
-            mantle = "#000000",
-            crust = "#000000",
-          },
-        },
-      }
-      vim.cmd.colorscheme "catppuccin-mocha"
+      require("kanso").load "zen"
     end,
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup {
+  --       auto_integrations = true,
+  --       color_overrides = {
+  --         mocha = {
+  --           base = "#000000",
+  --           mantle = "#000000",
+  --           crust = "#000000",
+  --         },
+  --       },
+  --     }
+  --     vim.cmd.colorscheme "catppuccin-mocha"
+  --   end,
+  -- },
   -- "EdenEast/nightfox.nvim",
   -- "sainnhe/gruvbox-material",
   -- "ellisonleao/gruvbox.nvim",
